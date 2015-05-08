59309a748818992b1c34e0162606f59946d58837
tomasjuri <tomasjuri@gmail.com>
2015-05-08 15:29:24 +0200

    rotace políček

--- src/ija/game/GUI.java
+++ src/ija/game/GUI.java
@@ -41,33 +41,65 @@ public class GUI {
                 ImageIcon icon;
                 switch (field.getCard().get_type()) {
                     case "C":
-                        switch(field.getCard().rotation) {
+                        switch(field.getCard().getRotation()) {
                             case 0: 
-                                icon = new ImageIcon(this.getClass().getResource("images/L0.jpg"));
+                                icon = new ImageIcon(this.getClass().getResource("images/C0.jpg"));
                                 break;
-                            case 1: 
-                                icon = new ImageIcon(this.getClass().getResource("images/L1.jpg"));
+                            case 90: 
+                                icon = new ImageIcon(this.getClass().getResource("images/C90.jpg"));
                                 break;
-                            case 2: 
-                                icon = new ImageIcon(this.getClass().getResource("images/L2.jpg"));
+                            case 180: 
+                                icon = new ImageIcon(this.getClass().getResource("images/C180.jpg"));
                                 break;
-                            case 3: 
-                                icon = new ImageIcon(this.getClass().getResource("images/L3.jpg"));
+                            case 270: 
+                                icon = new ImageIcon(this.getClass().getResource("images/C270.jpg"));
                                 break;
                             default:
-                                icon = new ImageIcon(this.getClass().getResource("images/L0.jpg"));
+                                icon = new ImageIcon(this.getClass().getResource("images/C0.jpg"));
                                 break;
                         }
                         label.setIcon(icon);
                         GamePanel.add(label);
                         break;
                     case "F":
-                        icon = new ImageIcon(this.getClass().getResource("images/T.jpg"));
+                        switch(field.getCard().getRotation()) {
+                            case 0: 
+                                icon = new ImageIcon(this.getClass().getResource("images/T0.jpg"));
+                                break;
+                            case 90: 
+                                icon = new ImageIcon(this.getClass().getResource("images/T90.jpg"));
+                                break;
+                            case 180: 
+                                icon = new ImageIcon(this.getClass().getResource("images/T180.jpg"));
+                                break;
+                            case 270: 
+                                icon = new ImageIcon(this.getClass().getResource("images/T270.jpg"));
+                                break;
+                            default:
+                                icon = new ImageIcon(this.getClass().getResource("images/T0.jpg"));
+                                break;
+                        }
                         label.setIcon(icon);
                         GamePanel.add(label);
                         break;
                     case "L":
-                        icon = new ImageIcon(this.getClass().getResource("images/I.jpg"));;
+                        switch(field.getCard().getRotation()) {
+                            case 0: 
+                                icon = new ImageIcon(this.getClass().getResource("images/L0.jpg"));
+                                break;
+                            case 90: 
+                                icon = new ImageIcon(this.getClass().getResource("images/L90.jpg"));
+                                break;
+                            case 180: 
+                                icon = new ImageIcon(this.getClass().getResource("images/L0.jpg"));
+                                break;
+                            case 270: 
+                                icon = new ImageIcon(this.getClass().getResource("images/L90.jpg"));
+                                break;
+                            default:
+                                icon = new ImageIcon(this.getClass().getResource("images/L0.jpg"));
+                                break;
+                        }
                         label.setIcon(icon);
                         GamePanel.add(label);
                         break;
