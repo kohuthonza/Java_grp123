153ef1865448d340e1733ce981b44245e73f259e
kohuthonza <kohut.honza@seznam.cz>
2015-05-14 16:37:35 +0200

    update

---
+++ lib/get-libs.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+rm ./images.zip ./images.zip.*
+
+wget https://www.dropbox.com/s/i37yo43w2nmvfvy/images.zip -P 
+
+unzip ./images.zip -d 
--- src/ija/game/download_images.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-rm ./lib/images.zip ./lib/images.zip.*
-
-wget https://www.dropbox.com/s/i37yo43w2nmvfvy/images.zip -P ./lib/
-
-unzip ./lib/images.zip -d ./lib
